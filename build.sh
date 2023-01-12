de14d2b56f076354d159ab1762b5c40abfde243c
dmitry <dzmitry.zenin@gmail.com>
2023-01-12 21:45:26 +0100

    minor

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VASM_HOME=~/Downloads/vasm/
-$VASM_HOME/vasm6502_oldstyle -Fbin -dotdir blink.65s
+$VASM_HOME/vasm6502_oldstyle -Fbin -dotdir counter.65s
 printf "\na.out contents\n\n"
 hexdump -C a.out
